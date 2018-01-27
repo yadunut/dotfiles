25377707789f83366b8b3b979bfa2ce0ae92bdca
Yadunand <yadunut@gmail.com>
2018-01-28 04:00:13 +0800

    Finished up install script

--- osx-installer.sh
+++ osx-installer.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 #__   __        _                             _   ______
 #\ \ / /       | |                           | |  | ___ \
@@ -12,6 +13,12 @@
 
 # Ask for sudo
 sudo -v
+
+#Keep Sudo Alive
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#Install Xcode CLI
+xcode-select --install
 
 
 ################################### Homebrew ###################################
@@ -101,6 +108,40 @@ ln -s -F ~/dev/dotfiles/tmux.conf ~/.tmux.conf
 
 #Change default shell to zsh
 chsh -s /bin/zsh
+
+#System Pref Settings
+
+# Set a faster keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+# Set a short delay until repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
+# require password immediately after sleep or screensaver
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "genie"
+
+# Bottom right screen corner → Desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+for app in "Dock" "Finder" "SystemUIServer" "Terminal"; do
+	killall "${app}" > /dev/null 2>&1
+done
+
 
 #Messages for the user
 echo "Remember to set terminal font to Inconsolata for Powerline"
