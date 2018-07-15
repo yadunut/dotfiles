dbd5b6df4c4944854987405f605be44571faffbe
Yadunand Prem <yadunut@gmail.com>
2018-07-15 21:13:01 +0800

    Update search function for server IP

--- oh-my-zsh-custom/XU4.zsh
+++ oh-my-zsh-custom/XU4.zsh
@@ -1,7 +1,7 @@
 function mntXU4 () {
     mkdir -p /Volumes/XU4
     pw=$(security find-internet-password -wl XU4)
-    server_ip=$(security find-internet-password -l XU4 | grep srvr | sed s/srvr// | sed s/\"//g | sed s/\<blob\>=//g| tr -d " \t\n\r" )
+    server_ip=$(security find-internet-password -l XU4 | grep -E "(?:\d{1,3}\.){3}\d{1,3}" -o)
     mount -t afp afp://yadunut:$pw@$server_ip/XU4-Files /Volumes/XU4/
 }
 
