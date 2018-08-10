3406ab1e2ba72bb44d79b8519ca2d088bb394279
Yadunand Prem <yadunut@gmail.com>
2018-08-10 16:22:39 +0800

    Nuked Everything

--- oh-my-zsh-custom/alias.zsh
+++
@@ -1,8 +0,0 @@
-alias c="clear"
-alias reload="source ~/.zshrc"
-alias t="tree"
-alias copypwd="pwd | pbcopy"
-alias h="cd ~/dev/src/github.com/yadunut"
-alias gclone="ruby ~/dev/src/github.com/yadunut/gclone/main.rb"
-alias vf='vim $(fzf)'
-alias git='hub'
--- oh-my-zsh-custom/exports.zsh
+++
@@ -1,9 +0,0 @@
-# Exports for go
-export GOPATH=$HOME/dev
-export PATH="/usr/local/sbin:$PATH"
-export PATH=$GOPATH/bin:$PATH
-export PATH="/usr/local/opt/openssl/bin:$PATH"
-export PATH=$HOME/dev/src/github.com/flutter/bin:$PATH
-
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
